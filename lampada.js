f0636ccf30626b2e4eb8b784781bbbf8baf0848b
ElielNascimento <nascimentob.eliel@gmail.com>
2022-05-11 13:27:16 -0300

    projeto lampada js purp

---
+++ lampada.js
@@ -0,0 +1,18 @@
+const lampOn = document.getElementById('lampOn');
+const lampOff = document.getElementById('lampOff');
+const lamp = document.getElementById('lamp');
+
+function ligar (){
+    lamp.src = './img/lampada-ligada.jpg'
+}
+
+function desligar (){
+    lamp.src = './img/lampada.jpg'
+}
+
+
+
+lampOn.addEventListener('click', ligar);
+lampOff.addEventListener('click', desligar);
+
+console.log(desligar)
